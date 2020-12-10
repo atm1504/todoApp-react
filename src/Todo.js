67693570f2e003bc2544d7f9d906cbe443d88173
Amartya Mondal <atm1504.in@gmail.com>
2020-12-11 00:23:20 +0530

    CHeck

--- src/Todo.js
+++ src/Todo.js
@@ -7,18 +7,22 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 
-function Todo({task, completed}) {
+function Todo({ task, completed, removeTodo, id, toggleTodo }) {
     return (
         <ListItem>
             <ListItemText style={{textDecoration:completed?"line-through":"none"}}>
-                <Checkbox tabIndex={-1} checked={completed}/>
+                <Checkbox
+                    tabIndex={-1}
+                    checked={completed}
+                    onClick={() => toggleTodo(id)}
+                />
                 {task}
             </ListItemText>
             <ListItemSecondaryAction>
-                <IconButton aria-label="Delete">
+                <IconButton aria-label="Delete" onClick={()=> removeTodo(id)}>
                     <DeleteIcon/>
                 </IconButton>
-                <IconButton aria-label="Edit">
+                <IconButton aria-label="Edit" >
                     <EditIcon/>
                 </IconButton>
             </ListItemSecondaryAction>
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,6 +6,7 @@ import ToolBar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
+import uuid from "uuid/dist/v4";
 
 
 function TodoApp() {
@@ -16,8 +17,21 @@ function TodoApp() {
     ];
     const [todos, setTodos] = useState(initialTodos);
     const addTodo = newTodoText => {
-        setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+        setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
     }
+
+    const removeTodo = todoId => {
+        const updatedTodos = todos.filter(todo => todo.id !== todoId);
+        setTodos(updatedTodos);
+    }
+
+    const toggleTodo = todoId => {
+        const updatedTodos = todos.map(todo =>
+            todo.id === todoId ? {...todo, completed: !todo.completed}:todo
+        );
+        setTodos(updatedTodos);
+    }
+
     return (
         <Paper style={{
             padding: 0,
@@ -36,7 +50,11 @@ function TodoApp() {
             <Grid container justify='center' style={{marginTop: "1rem"}}>
                 <Grid item xs={11} ms={8} g={4}>
                     <TodoForm addTodo={addTodo}/>
-                    <TodoList todos={todos} />
+                    <TodoList
+                        todos={todos}
+                        removeTodo={removeTodo}
+                        toggleTodo ={toggleTodo}
+                    />
                 </Grid>
             </Grid>
         </Paper>
